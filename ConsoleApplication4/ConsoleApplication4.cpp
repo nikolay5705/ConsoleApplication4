a45287dcc11f8f878c79c54afad13903f4f8fcc7
anzhelika_moroz <anzhelika_moroz@DESKTOP-IMBJFMC>
2023-09-30 22:25:48 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,41 @@
+﻿
+
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+int main()
+{
+
+    cout << "Hello" << endl;
+    cout << "Hello!!!" << endl;
+
+    cout << endl << endl << endl << endl;
+
+
+    /*cout << "SQUARE" << endl;
+    cout << " ***********" << endl;
+    cout << " *         *" << endl;
+    cout << " *         *" << endl;
+    cout << " *         *" << endl;
+    cout << " *         *" << endl;
+    cout << " ***********" << endl;*/
+
+    cout << endl << endl << endl << endl;
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "       Прізвище:Moroz" << endl;
+    cout << "           Ім*я:Mykola" << endl;
+    cout << "    По батькові:Oleksandrovich" << endl;
+    cout << "          Стать: чоловіча" << endl;
+    cout << "Дата народження: 2006-10-18" << endl;
+    cout << "         Адреса: П.Калнишевського 33/24" << endl;
+    cout << " Номер телефону: +380968701427" << endl;
+    cout << " Місце навчання: ФПМ,ДНУ ім. Олеся Гончара" << endl;
+    cout << "          Група: ПА-23-1" << endl;
+    cout << "          Хоббі:Програмування,шахи" << endl;
+  
+    system("pause");
+}
+
